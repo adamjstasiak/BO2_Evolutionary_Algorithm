77174b37af419cb3a39699755f088b2169fe342b
Szymon Szewczyk <szewczykszym@student.agh.edu.pl>
2022-12-23 19:43:18 +0100

    Plotting function flow graph

--- gui.py
+++ gui.py
@@ -1,5 +1,8 @@
 import tkinter as tk
 from tkinter import ttk
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 
 class tkinterApp(tk.Tk):
@@ -22,10 +25,10 @@ class tkinterApp(tk.Tk):
 
         # iterating through a tuple consisting
         # of the different page layouts
-        for F in (Parameters, FunctionFlowGraph, Page2):
+        for F in (Parameters, ChooseParcels, FunctionFlowGraph):
             frame = F(container, self)
             # initializing frame of that object from
-            # Parameters, FunctionFlowGraph, page2 respectively with
+            # Parameters, ChooseParcels, FunctionFlowGraph respectively with
             # for loop
             self.frames[F] = frame
             frame.grid(row=0, column=0, sticky="nsew")
@@ -38,23 +41,27 @@ class tkinterApp(tk.Tk):
         frame = self.frames[cont]
         frame.tkraise()
 
+    def get_page(self, page_class):
+        return self.frames[page_class]
+
 
 # first window frame Parameters
 class Parameters(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
+        self.controller = controller
 
         # Changing main frames buttons
         param_frame = ttk.Button(self, text="Parameters",
                                  command=lambda: controller.show_frame(Parameters))
         param_frame.grid(row=0, column=0, padx=5, pady=5)
 
-        chart_frame = ttk.Button(self, text="Function Flow Chart",
-                                 command=lambda: controller.show_frame(FunctionFlowGraph))
+        chart_frame = ttk.Button(self, text="Choose Parcels",
+                                 command=lambda: controller.show_frame(ChooseParcels))
         chart_frame.grid(row=0, column=1, padx=5, pady=5)
 
-        button2 = ttk.Button(self, text="Page 2",
-                             command=lambda: controller.show_frame(Page2))
+        button2 = ttk.Button(self, text="Function Flow Graph",
+                             command=lambda: controller.show_frame(FunctionFlowGraph))
         button2.grid(row=0, column=2, padx=5, pady=5)
 
         # Main parameters frame
@@ -66,44 +73,44 @@ class Parameters(tk.Frame):
         pop_size_str = ttk.Label(label_frame, text='Population size')
         pop_size_str.grid(row=0, column=0, padx=5, pady=3)
 
-        population_size = tk.IntVar()
-        population_size.trace("w", lambda name, index,
-                              mode, sv=population_size: self.callback(sv))
+        self.population_size = tk.IntVar()
+        self.population_size.trace("w", lambda name, index,
+                                   mode, sv=self.population_size: self.callback(sv))
         pop_entry = ttk.Entry(
-            label_frame, textvariable=population_size, justify='right')
+            label_frame, textvariable=self.population_size, justify='right')
         pop_entry.grid(row=0, column=1, padx=5, pady=3)
 
         # Parent percentage row
         parent_str = ttk.Label(label_frame, text='Parents [%]')
         parent_str.grid(row=1, column=0, padx=5, pady=3)
 
-        parent_percentage = tk.IntVar()
-        parent_percentage.trace("w", lambda name, index,
-                                mode, sv=parent_percentage: self.callback(sv))
+        self.parent_percentage = tk.IntVar()
+        self.parent_percentage.trace("w", lambda name, index,
+                                     mode, sv=self.parent_percentage: self.callback(sv))
         par_entry = ttk.Entry(
-            label_frame, textvariable=parent_percentage, justify='right')
+            label_frame, textvariable=self.parent_percentage, justify='right')
         par_entry.grid(row=1, column=1, padx=5, pady=3)
 
         # Mutation percentage row
         mutation_str = ttk.Label(label_frame, text='Mutation chance [%]')
         mutation_str.grid(row=2, column=0, padx=5, pady=3)
 
-        mutation_percentage = tk.IntVar()
-        mutation_percentage.trace("w", lambda name, index,
-                                  mode, sv=mutation_percentage: self.callback(sv))
+        self.mutation_percentage = tk.IntVar()
+        self.mutation_percentage.trace("w", lambda name, index,
+                                       mode, sv=self.mutation_percentage: self.callback(sv))
         mut_entry = ttk.Entry(
-            label_frame, textvariable=mutation_percentage, justify='right')
+            label_frame, textvariable=self.mutation_percentage, justify='right')
         mut_entry.grid(row=2, column=1, padx=5, pady=3)
 
         # Number of generations row
         generations_str = ttk.Label(label_frame, text='Number of generations')
         generations_str.grid(row=3, column=0, padx=5, pady=3)
 
-        number_of_generations = tk.IntVar()
-        number_of_generations.trace("w", lambda name, index,
-                                    mode, sv=number_of_generations: self.callback(sv))
+        self.number_of_generations = tk.IntVar()
+        self.number_of_generations.trace("w", lambda name, index,
+                                         mode, sv=self.number_of_generations: self.callback(sv))
         num_of_gen_entry = ttk.Entry(
-            label_frame, textvariable=number_of_generations, justify='right')
+            label_frame, textvariable=self.number_of_generations, justify='right')
         num_of_gen_entry.grid(row=3, column=1, padx=5, pady=3)
 
         # Mutation and Crossover frame
@@ -116,49 +123,49 @@ class Parameters(tk.Frame):
         crossover_frame.grid(row=4, column=1, padx=5, pady=5)
 
         # Mutation frame (swap, inversion, scramble)
-        swap_mutation = tk.IntVar()
-        swap_mutation.trace("w", lambda name, index,
-                            mode, sv=swap_mutation: self.callback(sv))
+        self.swap_mutation = tk.IntVar()
+        self.swap_mutation.trace("w", lambda name, index,
+                                 mode, sv=self.swap_mutation: self.callback(sv))
         swap_mutation_button = ttk.Checkbutton(mutation_frame, text='Swap',
-                                               variable=swap_mutation)
+                                               variable=self.swap_mutation)
         swap_mutation_button.grid(row=0, column=0, padx=5, pady=2, sticky='w')
 
-        inverse_mutation = tk.IntVar()
-        inverse_mutation.trace("w", lambda name, index,
-                               mode, sv=inverse_mutation: self.callback(sv))
+        self.inverse_mutation = tk.IntVar()
+        self.inverse_mutation.trace("w", lambda name, index,
+                                    mode, sv=self.inverse_mutation: self.callback(sv))
         inverse_mutation_button = ttk.Checkbutton(mutation_frame, text='Inverse',
-                                                  variable=inverse_mutation)
+                                                  variable=self.inverse_mutation)
         inverse_mutation_button.grid(
             row=1, column=0, padx=5, pady=2, sticky='w')
 
-        scramble_mutation = tk.IntVar()
-        scramble_mutation.trace("w", lambda name, index,
-                                mode, sv=scramble_mutation: self.callback(sv))
+        self.scramble_mutation = tk.IntVar()
+        self.scramble_mutation.trace("w", lambda name, index,
+                                     mode, sv=self.scramble_mutation: self.callback(sv))
         scramble_mutation_button = ttk.Checkbutton(mutation_frame, text='Scramble',
-                                                   variable=scramble_mutation)
+                                                   variable=self.scramble_mutation)
         scramble_mutation_button.grid(
             row=3, column=0, padx=5, pady=2, sticky='w')
 
         # Crossover (PMX, OX, CX)
-        PMX_crossover = tk.IntVar()
-        PMX_crossover.trace("w", lambda name, index,
-                            mode, sv=PMX_crossover: self.callback(sv))
+        self.PMX_crossover = tk.IntVar()
+        self.PMX_crossover.trace("w", lambda name, index,
+                                 mode, sv=self.PMX_crossover: self.callback(sv))
         PMX_crossover_button = ttk.Checkbutton(crossover_frame, text='PMX',
-                                               variable=PMX_crossover)
+                                               variable=self.PMX_crossover)
         PMX_crossover_button.grid(row=0, column=0, padx=5, pady=2, sticky='w')
 
-        OX_crossover = tk.IntVar()
-        OX_crossover.trace("w", lambda name, index,
-                           mode, sv=OX_crossover: self.callback(sv))
+        self.OX_crossover = tk.IntVar()
+        self.OX_crossover.trace("w", lambda name, index,
+                                mode, sv=self.OX_crossover: self.callback(sv))
         OX_crossover_button = ttk.Checkbutton(crossover_frame, text='OX',
-                                              variable=OX_crossover)
+                                              variable=self.OX_crossover)
         OX_crossover_button.grid(row=1, column=0, padx=5, pady=2, sticky='w')
 
-        CX_crossover = tk.IntVar()
-        CX_crossover.trace("w", lambda name, index,
-                           mode, sv=CX_crossover: self.callback(sv))
+        self.CX_crossover = tk.IntVar()
+        self.CX_crossover.trace("w", lambda name, index,
+                                mode, sv=self.CX_crossover: self.callback(sv))
         CX_crossover_button = ttk.Checkbutton(crossover_frame, text='CX',
-                                              variable=CX_crossover)
+                                              variable=self.CX_crossover)
         CX_crossover_button.grid(row=3, column=0, padx=5, pady=2, sticky='w')
 
         # Selection Frame and buttons (Roulette, Ranking)
@@ -166,65 +173,104 @@ class Parameters(tk.Frame):
             label_frame, text="Selection", width=100, height=50)
         selection_frame.grid(row=5, column=0, padx=5, pady=5, columnspan=2)
 
-        selection = tk.StringVar()
-        selection.trace("w", lambda name, index,
-                        mode, sv=selection: self.callback(sv))
+        self.selection = tk.StringVar()
+        self.selection.trace("w", lambda name, index,
+                             mode, sv=self.selection: self.callback(sv))
         roulette_selection_radiobutton = ttk.Radiobutton(
-            selection_frame, text='Roulette', variable=selection, value='roulette')
+            selection_frame, text='Roulette', variable=self.selection, value='roulette')
         roulette_selection_radiobutton.grid(row=0, column=0, padx=5, pady=2)
 
         ranking_selection_radiobutton = ttk.Radiobutton(
-            selection_frame, text='Ranking', variable=selection, value='ranking')
+            selection_frame, text='Ranking', variable=self.selection, value='ranking')
         ranking_selection_radiobutton.grid(row=0, column=1, padx=5, pady=2)
+
+        # Start
+        s = ttk.Style()
+        s.configure('my.TButton', font=('Helvetica', 12))
+        self.start = ttk.Button(label_frame, text="Start", style='my.TButton',
+                                command=self.start_algorithm)
+        self.start.grid(row=6, column=0, padx=5, pady=5, columnspan=2)
 
     def callback(self, sv):
         """Return value of entered value"""
         return print(sv._name, sv.get())
 
+    def start_algorithm(self):
+        graph_page = self.controller.get_page(FunctionFlowGraph)
+        graph_page.plot_dataframe(graph_page.canvas, graph_page.ax, df)
 
-# second window frame FunctionFlowGraph
+
+# second window frame ChooseParcels
+class ChooseParcels(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+
+        label = ttk.Label(self, text="Choose Parcels")
+        label.grid(row=1, column=1, padx=5, pady=5)
+
+        param_frame = ttk.Button(self, text="Parameters",
+                                 command=lambda: controller.show_frame(Parameters))
+        param_frame.grid(row=0, column=0, padx=5, pady=5)
+
+        chart_frame = ttk.Button(self, text="Choose Parcels",
+                                 command=lambda: controller.show_frame(ChooseParcels))
+        chart_frame.grid(row=0, column=1, padx=5, pady=5)
+
+        button2 = ttk.Button(self, text="Function Flow Graph",
+                             command=lambda: controller.show_frame(FunctionFlowGraph))
+        button2.grid(row=0, column=2, padx=5, pady=5)
+
+
+# third window frame FunctionFlowGraph
 class FunctionFlowGraph(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
+        self.controller = controller
 
         param_frame = ttk.Button(self, text="Parameters",
                                  command=lambda: controller.show_frame(Parameters))
         param_frame.grid(row=0, column=0, padx=5, pady=5)
 
-        chart_frame = ttk.Button(self, text="Function Flow Chart",
-                                 command=lambda: controller.show_frame(FunctionFlowGraph))
+        chart_frame = ttk.Button(self, text="Choose Parcels",
+                                 command=lambda: controller.show_frame(ChooseParcels))
         chart_frame.grid(row=0, column=1, padx=5, pady=5)
 
-        button2 = ttk.Button(self, text="Page 2",
-                             command=lambda: controller.show_frame(Page2))
+        button2 = ttk.Button(self, text="Function Flow Graph",
+                             command=lambda: controller.show_frame(FunctionFlowGraph))
         button2.grid(row=0, column=2, padx=5, pady=5)
 
-        # label of frame Layout 2
-        label = ttk.Label(self, text="Function Flow Chart")
-        label.grid(row=1, column=1, padx=5, pady=5)
+        s = ttk.Style()
+        s.configure('My.TFrame', background='green')
+        self.plot_frame = ttk.Frame(
+            self, style='My.TFrame', width=20, height=20)
+        self.plot_frame.grid(row=1, padx=5, pady=5, columnspan=4)
+
+        # Space for graph
+        figure = plt.Figure(figsize=(8, 6), dpi=100)
+        self.ax = figure.add_axes([0.1, 0.1, 0.8, 0.8])
+        self.canvas = FigureCanvasTkAgg(figure, self.plot_frame)
+        self.canvas.get_tk_widget().grid(row=0, column=1)
+        self.canvas.draw()
+
+    def plot_dataframe(self, canvas, ax, df):
+        ax.clear()         # clear axes from previous plot
+        ax.plot(df.year, df.unemployment_rate)
+        canvas.draw()
 
 
-# third window frame page2
-class Page2(tk.Frame):
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-
-        label = ttk.Label(self, text="Page2")
-        label.grid(row=1, column=1, padx=5, pady=5)
-
-        param_frame = ttk.Button(self, text="Parameters",
-                                 command=lambda: controller.show_frame(Parameters))
-        param_frame.grid(row=0, column=0, padx=5, pady=5)
-
-        chart_frame = ttk.Button(self, text="Function Flow Chart",
-                                 command=lambda: controller.show_frame(FunctionFlowGraph))
-        chart_frame.grid(row=0, column=1, padx=5, pady=5)
-
-        button2 = ttk.Button(self, text="Page 2",
-                             command=lambda: controller.show_frame(Page2))
-        button2.grid(row=0, column=2, padx=5, pady=5)
+df = {'year': [1920, 1930, 1940, 1950, 1960, 1970, 1980, 1990, 2000, 2010],
+      'unemployment_rate': [9.8, 12, 8, 7.2, 6.9, 7, 6.5, 6.2, 5.5, 6.3]
+      }
+df = pd.DataFrame(df)
 
 
-# Driver Code
-app = tkinterApp()
-app.mainloop()
+def main():
+
+    app = tkinterApp()
+    # print('bbbbb', app.get_page(Parameters).population_size.get())
+    app.mainloop()
+
+
+if __name__ == "__main__":
+    main()
